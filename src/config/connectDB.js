2e15f88003cc830a53010cc58f6bd6bcd9e5814e
Ha Tri Duc <hatriduc32@gmail.com>
2023-03-31 02:57:41 +0700

    update connect db

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,10 +1,11 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    try {
-        await mongoose.connect('mongodb://127.0.0.1/tevily_sever');
-        console.log('connect success')
-    } catch (e) {
-        console.log('connect failed')
-    }
-}
+  try {
+    await mongoose.connect(process.env.DB_URL);
+    // await mongoose.connect('mongodb://127.0.0.1/tevily_sever');
+    console.log("connect success");
+  } catch (e) {
+    console.log("connect failed");
+  }
+};
